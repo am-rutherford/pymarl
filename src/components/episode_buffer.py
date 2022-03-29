fd7154233a9cd139c21828ff05203cf211a6e52a
am-rutherford <66562395+am-rutherford@users.noreply.github.com>
2022-03-29 10:11:28 +0100

    results and models from main

--- src/components/episode_buffer.py
+++ src/components/episode_buffer.py
@@ -130,7 +130,7 @@ class EpisodeBatch:
         for s in dest.shape[::-1]:
             if v.shape[idx] != s:
                 if s != 1:
-                    raise ValueError("Unsafe reshape of {} to {}".format(v.shape, dest.shape))
+                    raise ValueError("Unsafe reshape of {} to {}, v: {}, destL {}".format(v.shape, dest.shape, v, dest))
             else:
                 idx -= 1
 
